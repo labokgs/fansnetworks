4829050405b8b0cc0399168d91094d63aede2d23
tomoyam7 <tomo.k.messi@gmail.com>
2022-10-22 12:53:12 +0900

    CustomUserモデルをmigrate

---
+++ fansnetworks/settings_dev.py
@@ -0,0 +1,53 @@
+from pathlib import Path
+from .local_settings import *
+from .settings_common import *
+
+import os
+
+# SECURITY WARNING: keep the secret key used in production secret!
+
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': 'INFO',
+        } 
+    },
+    'diary': {
+        'console': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+        },
+    },
+    'handlers': {
+        'console':{
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'dev'
+        },
+    },
+
+    'formatters':{
+        'dev': {
+            'format': '\t'.join([
+                '%(asctime)s',
+                '[%(levelname)s]',
+                '%(pathname)s(Line:%(lineno)d)',
+                '%(message)s'
+            ])
+        },
+    }
+
+}
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
--- fansnetworks/wsgi.py
+++ fansnetworks/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "fansnetworks.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "fansnetworks.settings_common")
 
 application = get_wsgi_application()
