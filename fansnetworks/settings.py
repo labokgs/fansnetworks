19be3a3ffade7048417877c0c88d138ce599ee1f
tomoyam7 <tomo.k.messi@gmail.com>
2022-11-16 15:50:08 +0900

    heroku settings.

--- fansnetworks/settings.py
+++ fansnetworks/settings.py
@@ -1,65 +1,36 @@
 from .settings_common import *
+from .local_settings import *
 
-#本番運用環境用にセキュリティキーを生成し環境変数から読み込み
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
-#デバッグモードを有効にするかどうか
 DEBUG = False
 
-#許可するホスト名のリスト
-ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOSTS')]
+try:
+    # 存在する場合、ローカルの設定読み込み
+    from .local_settings import *
+except ImportError:
+    pass
 
-#静的ファイルを配置する場所
-STATIC_ROOT = 'usr/share/nginx/html/static'
-MEDIA_ROOT = '/usr/share/nginx/html/media'
+if not DEBUG:
+    # Heroku settings
 
+    # staticの設定
+    import os
+    import django_heroku
 
-#Amazon SES関連設定
-AWS_SES_ACCESS_KEY_ID = os.environ.get('AWS_SES_ACCESS_KEY_ID')
-AWS_SES_SECRET_KEY = os.environ.get('AWS_SES_SECRET_KEY')
-EMAIL_BACKEND = 'django_ses.SESBackend'
+    BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+    # Static files (CSS, JavaScript, Images)
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    STATIC_URL = '/static/'
 
+    # Extra places for collectstatic to find static files.
+    STATICFILES_DIRS = (
+        os.path.join(BASE_DIR, 'static'),
+    )
 
-#ロギング
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
+    MIDDLEWARE += [
+        'whitenoise.middleware.WhiteNoiseMiddleware',
+    ]
 
-    #ロガーの設定
-    'loggers': {
-        #Djangoが利用するロガー
-        'django': {
-            'handlers': ['file'],
-            'level': 'INFO',
-        },
-        #diaryアプリケーションが利用するロガー
-        'diary': {
-            'handlers': ['file'],
-            'level': 'INFO',
-        },
-    },
-
-    'handlers': {
-        'file': {
-            'level': 'INFO',
-            'class': 'logging.handlers.TimedRotatingFileHandler',
-            'filename': os.path.join(BASE_DIR, 'logs/django.log'),
-            'formatter': 'prod',
-            'when': 'D', #ログローテーション（新しいファイルへの切り替え）感覚の単位（D=日）
-            'interval': 1, #ログローテーション感覚（1日単位）"
-            'backupCount': 7, #保存しておくログファイル数
-        },
-    },
-
-    'formatters': {
-        'prod': {
-            'format': '\t'.join([
-                '%(asctime)s',
-                '[%(levlename)s]',
-                '%(pathname)s(Line:%(lineno)d)',
-                '%(message)s'
-            ])
-        },
-    }
-}
+    # HerokuのConfigを読み込み
+    django_heroku.settings(locals())
